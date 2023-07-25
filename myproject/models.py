5418c8f27f365f568643207bc1e1e25fc6631d5a
ardaemre1 <123388990+ardaemre1@users.noreply.github.com>
2023-07-25 08:54:23 +0300

    First Commit

---
+++ myproject/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=100, verbose_name='Ürün Adı')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Fiyat')
+    description = models.TextField(verbose_name='Açıklama')
+    brand = models.CharField(max_length=50, verbose_name='Marka')
+    stock_status = models.BooleanField(default=True, verbose_name='Stok Durumu')
+    images = models.ImageField(upload_to='product_images/', verbose_name='Resimler')
+    stock_quantity = models.PositiveIntegerField(default=0, verbose_name='Stok Miktarı')
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name_plural = 'Ürünler'
